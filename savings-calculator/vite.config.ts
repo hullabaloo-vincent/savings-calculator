275aa55e5ce926d50f1111e018743548f7f125ce
VAliquo <vincentaliquo@gmail.com>
2025-08-01 21:58:16 -0400

    [FIX] Deploy

--- savings-calculator/vite.config.ts
+++ savings-calculator/vite.config.ts
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/savings-calculator/',
   server: {
     port: 3000,
     open: true
   },
   build: {
-    outDir: 'build',
+    outDir: 'dist',
     sourcemap: true
   },
   define: {
